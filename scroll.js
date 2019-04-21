c0179ad1d6785d11160534c787b7e03a0f944276
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-21 21:30:01 +0600

    no message

---
+++ scroll.js
@@ -0,0 +1,17 @@
+// 2019-04-21 https://github.com/segmentio/nightmare/issues/625#issuecomment-217730846
+const self = module.exports = {
+	execute:(n, cb) => {self.step(n, null, 0, cb);},
+	step:(n, prev, cur, cb) => {
+		if (prev === cur) {
+			cb();
+		}
+		else {
+			prev = cur;
+			n.evaluate(() => document.body.scrollHeight).then((v) => {
+				cur = v;
+				n.scrollTo(v, 0).wait(1000);
+				self.step(n, prev, cur, cb);
+			});
+		}
+	}
+};
