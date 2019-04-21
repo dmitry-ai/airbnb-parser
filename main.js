236c4f190f9c1131749ac0fe9404e7308bce187b
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-21 21:53:33 +0600

    no message

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // 2019-04-19
 const _ = require('lodash');
 const mFlat = require('./flat.js');
-const mPage = require('./page.js');
+const mNav = require('./nav.js');
 const initialURL = 'https://www.airbnb.com/s/homes?refinement_paths%5B%5D=%2Fhomes&adults=2&children=0&infants=0&toddlers=0&place_id=ChIJawhoAASnyhQR0LABvJj-zOE&query=Istanbul%2C%20Turkey&room_types%5B%5D=Entire%20home%2Fapt&amenities%5B%5D=25&amenities%5B%5D=8&amenities%5B%5D=5&amenities%5B%5D=33&amenities%5B%5D=4&amenities%5B%5D=47&min_beds=1&min_bedrooms=1&min_bathrooms=1&map_toggle=false&search_type=PAGINATION';
-mPage.execute(initialURL, (flats, next) => {
-	console.log('finish');
+mNav.execute(initialURL, (flats) => {
+	console.log(flats);
 });
---
+++ nav.js
@@ -0,0 +1,48 @@
+// 2019-04-21
+const mScroll = require('./scroll.js');
+const Nightmare = require('nightmare');
+const self = module.exports = {
+	execute:(url, cb) => {self.page(url, [], cb);}
+	,page:(url, result, cb) => {
+		const n = Nightmare({height: 1000, modal: false, openDevTools: true, show: true, width: 800});
+		n.goto(url);
+		n.wait('div[itemprop="itemListElement"]');
+		n.inject('js', 'lib/jquery-3.4.0.js');
+		n.evaluate(() => {jQuery.noConflict();});
+		mScroll.execute(n, () => {
+			n
+				.evaluate(() => {
+					return jQuery('div[itemprop=itemListElement] meta[itemprop=url]').map(function() {return(
+						'https://' + jQuery(this).attr('content').split('?')[0]
+					);}).get();
+				})
+				.then(flats => {
+					console.log('flats: ' + flats.length);
+					n.evaluate(() => {
+						var $ = jQuery;
+						var r = null;
+						var $a = $('a[aria-label*=Page]',
+							$('a[aria-label*=current]', $('ul[data-id=SearchResultsPagination]'))
+								.closest('li').next('li')
+						);
+						return !$a.length ? null : 'https://www.airbnb.com' + $a.attr('href');
+					}).then(next => {
+						n.end().then(() => {
+							result = result.concat(flats);
+							if (!next) {
+								cb(result);
+							}
+							else {
+								console.log('next: ' + next);
+								self.page(next, result, cb);
+							}
+						});
+					});
+				})
+				.catch(e => {
+					console.log(e);
+				})
+			;
+		});
+	}
+};
