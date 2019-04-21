2b891216442050e46e34933ec6ae827306c5ce11
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-21 22:04:34 +0600

    no message

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const _ = require('lodash');
 const mFlat = require('./flat.js');
 const mNav = require('./nav.js');
 const initialURL = 'https://www.airbnb.com/s/homes?refinement_paths%5B%5D=%2Fhomes&adults=2&children=0&infants=0&toddlers=0&place_id=ChIJawhoAASnyhQR0LABvJj-zOE&query=Istanbul%2C%20Turkey&room_types%5B%5D=Entire%20home%2Fapt&amenities%5B%5D=25&amenities%5B%5D=8&amenities%5B%5D=5&amenities%5B%5D=33&amenities%5B%5D=4&amenities%5B%5D=47&min_beds=1&min_bedrooms=1&min_bathrooms=1&map_toggle=false&search_type=PAGINATION';
+/*
 mNav.execute(initialURL, (flats) => {
 	console.log(flats);
+	_.each(flats.slice(0, 1), (flatURL) => {
+		mFlat.execute(flatURL, function(r) {
+			console.log(flatURL);
+			console.log(r);
+		})
+	});
+});
+*/
+mFlat.execute('https://www.airbnb.com/rooms/3443462', function(r) {
+	console.log(r);
 });
