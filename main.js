49f980280b6582d1a754ac176bc1735945e15f35
Dmitry Fedyuk <dfediuk@gmail.com>
2019-12-20 15:16:35 +0600

    no message

--- main.js
+++ main.js
@@ -3,14 +3,38 @@
 // An usage example:
 // node main \
 //	--imagesDir=C:/work/clients/nightmare/images-2019-05-18 \
-//	--maxPages=1 \
-//	--url="https://www.airbnb.com/s/homes?refinement_paths%5B%5D=%2Fhomes&adults=2&children=0&infants=0&toddlers=0&place_id=ChIJawhoAASnyhQR0LABvJj-zOE&query=Istanbul%2C%20Turkey&room_types%5B%5D=Entire%20home%2Fapt&amenities%5B%5D=25&amenities%5B%5D=8&amenities%5B%5D=5&amenities%5B%5D=33&amenities%5B%5D=4&amenities%5B%5D=47&min_beds=1&min_bedrooms=1&min_bathrooms=1&map_toggle=false&search_type=PAGINATION"
+//	--location="Bangkok, Thailand" \
+//	--maxPages=1
 const _ = require('lodash');
 const mFlats = require('./flats.js');
 const mNav = require('./nav.js');
 const mPath = require('path');
 const mShell = require('shelljs');
-mNav.execute(require('yargs').argv['url'], (flats) => {
+const mJP = require('jquery-param');
+const fixedParams = {
+	adults: 2
+	,amenities: [
+		4
+		,5
+		,8
+		,25
+		,33 // 2019-12-20 Washer
+		,46
+		,47
+		,78
+	]
+	,map_toggle: false
+	,min_bathrooms: 1
+	,min_bedrooms: 1
+	,min_beds: 1
+	,property_type_id: [1]
+	,refinement_paths: ['/homes']
+	,room_types: ['Entire home/apt']
+	,search_type: 'PAGINATION'
+	,toddlers: 0
+};
+const url = `https://www.airbnb.com/s/homes?${mJP(_.assign(fixedParams, {query: require('yargs').argv['location']}))}`;
+mNav.execute(url, (flats) => {
 	console.log('Flats: ' + flats.length);
 	const fBase = mPath.resolve(mPath.dirname(__dirname), 'images');
 	mShell.rm('-rf', fBase);
