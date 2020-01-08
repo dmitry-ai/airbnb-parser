9e57e7001b201af26aad688ad52fd5f824473f4c
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-08 07:03:09 +0600

    no message

--- mongo.js
+++ mongo.js
@@ -5,6 +5,9 @@
 // 3) @TODO https://mongoosejs.com
 const {MongoClient: mMongo} = require('mongodb');
 const assert = require('assert');
+// 2020-01-08
+// "How to fix «current Server Discovery and Monitoring engine is deprecated» on a MongoDB connection
+// using the `mongodb` Node.js package?": https://df.tips/t/973
 const client = new mMongo('mongodb://localhost:27017', {useUnifiedTopology: true});
 client.connect(function(e) {
 	assert.equal(null, e);
