28fd41161a92a715fcf35c0e3e81633781fdf48d
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-08 10:00:53 +0600

    no message

--- mongo.js
+++ mongo.js
@@ -3,14 +3,24 @@
 // https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
 // 2) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Assigning_to_new_variable_names
 // 3) @TODO https://mongoosejs.com
-const {MongoClient: mMongo} = require('mongodb');
-const assert = require('assert');
-// 2020-01-08
-// "How to fix «current Server Discovery and Monitoring engine is deprecated» on a MongoDB connection
-// using the `mongodb` Node.js package?": https://df.tips/t/973
-const client = new mMongo('mongodb://localhost:27017', {useUnifiedTopology: true});
-client.connect(function(e) {
-	assert.equal(null, e);
-	const db = client.db('airbnb');
-	client.close();
-});
+async function main() {
+	const {MongoClient: mMongo} = require('mongodb');
+	// 2020-01-08
+	// "How to fix «current Server Discovery and Monitoring engine is deprecated» on a MongoDB connection
+	// using the `mongodb` Node.js package?": https://df.tips/t/973
+	const mongo = new mMongo('mongodb://localhost:27017', {useUnifiedTopology: true});
+	try {
+		await mongo.connect();
+		const db = mongo.db('airbnb');
+		db.collection('inventory', function (err, c) {
+			c.find().toArray(function(err, items) {
+				if(err) throw err;
+				console.log(items);
+			});
+		});
+	}
+	finally {
+		mongo.close();
+	}
+}
+main().catch(console.error);
