9e2b98a20f94908177f8f725e1ad3c85e662d781
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-09 08:51:56 +0600

    no message

--- db.js
+++ db.js
@@ -1,9 +1,34 @@
 // 2020-01-09
 const _ = require('lodash');
-module.exports = {
-	connect() {}
-	,disconnect() {}
-	,save(d) {
-		debugger;
+const {MongoClient: mMongo} = require('mongodb');
+const self = module.exports = {
+	// 2020-01-08
+	// "How to fix «current Server Discovery and Monitoring engine is deprecated» on a MongoDB connection
+	// using the `mongodb` Node.js package?": https://df.tips/t/973
+	client: _.once(() => new mMongo('mongodb://localhost:27017', {useUnifiedTopology: true})),
+	//connect() {return mongo.connect();}
+	//,disconnect() {mongo.close();}
+	save(d) {
+		const db = self.client().db('airbnb');
+		/**
+		 * 2020-01-09
+		 * 1) https://github.com/mongodb/node-mongodb-native/blob/v3.4.1/lib/db.js#L387-L479
+		 * http://mongodb.github.io/node-mongodb-native/3.4/api/Db.html#collection
+		 * «Fetch a specific collection (containing the actual collection information)».
+		 * 2) «What is MongoDBs strict mode and is it a good idea to use?» https://stackoverflow.com/a/21595828
+		 * 3) Collection::find():
+		 * «Creates a cursor for a query that can be used to iterate over results from MongoDB».
+		 * http://mongodb.github.io/node-mongodb-native/3.4/api/Collection.html#find
+		 * 3.1) See also: https://docs.mongodb.com/v4.2/reference/method/db.collection.find
+		 * 4) Cursor::toArray():
+		 * «Returns an array of documents.
+		 * The caller is responsible for making sure that there is enough memory to store the results.
+		 * Note that the array only contains partial results when this cursor had been previously accessed.
+		 * In that case, cursor.rewind() can be used to reset the cursor.»
+		 * http://mongodb.github.io/node-mongodb-native/3.4/api/Cursor.html#toArray
+		 */
+		const cFlat = db.collection('flat');
+		d._id = d.id;
+		return cFlat.insertOne(d);
 	}
 };
--- flats.js
+++ flats.js
@@ -9,9 +9,12 @@ const fBase = mConfig.imagesDir();
 const self = module.exports = {
 	execute(flats, cb) {
 		mShell.rm('-rf', fBase);
-		mDB.connect();
-		try {self.step(flats, cb);}
-		finally {mDB.disconnect();}
+		mDB.client().connect().then(() => {
+			self.step(flats, () => {
+				mDB.client().close();
+				cb();
+			});
+		});
 	}
 	,step(flats, cb) {
 		if (!flats.length) {
@@ -22,22 +25,23 @@ const self = module.exports = {
 			mFlat.execute(flat, function(d) {
 				const flatId = d['id'];
 				console.log('Flat: ' + flatId);
-				mDB.save(d);
-				var fFlat = fBase + '/' + flatId;
-				mShell.rm('-rf', fFlat);
-				mShell.mkdir('-p', fFlat);
-				const images = _.mapValues(
-					_.keyBy(_.map(d['photos'], p => {return _.pick(p, ['id', 'xx_large']);}), 'id'), 'xx_large'
-				);
-				//const owner = flat['primary_host'];
-				// 2020-01-09 A photo URL looks like https://a0.muscache.com/im/pictures/<GUID>.jpg?aki_policy=xx_large
-				_.each(images, (url, imageId) => {
-					mDownloader
-						.image({dest: fFlat + '/' + imageId + '.jpeg', url: url})
-						.then(({filename}) => {console.log(filename);})
-					;
+				mDB.save(d).then(() => {
+					var fFlat = fBase + '/' + flatId;
+					mShell.rm('-rf', fFlat);
+					mShell.mkdir('-p', fFlat);
+					const images = _.mapValues(
+						_.keyBy(_.map(d['photos'], p => {return _.pick(p, ['id', 'xx_large']);}), 'id'), 'xx_large'
+					);
+					//const owner = flat['primary_host'];
+					// 2020-01-09 A photo URL looks like https://a0.muscache.com/im/pictures/<GUID>.jpg?aki_policy=xx_large
+					_.each(images, (url, imageId) => {
+						mDownloader
+							.image({dest: fFlat + '/' + imageId + '.jpeg', url: url})
+							.then(({filename}) => {console.log(filename);})
+						;
+					});
+					self.step(flats, cb);
 				});
-				self.step(flats, cb);
 			});
 		}
 	}
