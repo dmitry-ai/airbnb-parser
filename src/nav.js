25bdab8375e70155c2f7e1177a41bdfde64423e9
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-11 12:01:05 +0600

    no message

--- src/nav.js
+++ src/nav.js
@@ -3,24 +3,32 @@
 const mConfig = require('./config.js');
 const mScroll = require('./scroll.js');
 const Nightmare = require('nightmare'); // 2020-01-10 https://github.com/segmentio/nightmare
-var curPage = 0;
-const maxPages = mConfig.maxPages();
 module.exports = {
 	/**
 	 * 2020-01-10
 	 * @used-by main.js
 	 * @param {string} url
-	 * @return {Promise<void>}
+	 * @return {string[]}
 	 */
-	async execute(url) {return this.page(url, []);}
+	async execute(url) {
+		const maxPages = mConfig.maxPages();
+		var curPage = 1, items, r = [];
+		while (url && curPage <= maxPages) {
+			console.log(`Page ${curPage}: ${url}`);
+			([items, url] = await this.page(url));
+			r = r.concat(items);
+			curPage++;
+		}
+		return r;
+	}
+
 	/**
 	 * 2020-01-10
 	 * @private
 	 * @used-by execute()
 	 * @param {string} url
-	 * @param {string[]} r
 	 */
-	,async page(url, r) {
+	,async page(url) {
 		const n = Nightmare({
 			height: 1000, modal: false, openDevTools: mConfig.openDevTools(), show: mConfig.show(), width: 800
 		});
@@ -32,29 +40,24 @@ module.exports = {
 		// https://github.com/segmentio/nightmare/blob/3.0.2/lib/actions.js#L611-L642
 		n.evaluate(() => jQuery.noConflict());
 		await mScroll.execute(n);
-		const flats = await n.evaluate(() =>
-			// 2020-01-10 The arrow function syntax does not work inside evaluate()
-			jQuery('div[itemprop=itemListElement] meta[itemprop=url]').map(function() {return(
-				'https://' + jQuery(this).attr('content').split('?')[0]
-					// 2019-12-20 The URL now contains «undefined» or «null» instead of «www.airbnb.com».
-					.replace(/^undefined|null/, 'www.airbnb.com')
+		const r = [
+			await n.evaluate(() =>
+				// 2020-01-10 The arrow function syntax does not work inside evaluate()
+				jQuery('div[itemprop=itemListElement] meta[itemprop=url]').map(function() {return(
+					'https://' + jQuery(this).attr('content').split('?')[0]
+						// 2019-12-20 The URL now contains «undefined» or «null» instead of «www.airbnb.com».
+						.replace(/^undefined|null/, 'www.airbnb.com')
 			);}).get())
-		;
-		const next = await n.evaluate(() => {
-			var $ = jQuery;
-			var $a = $('a[aria-label*=Page]',
-				$('a[aria-label*=current]', $('ul[data-id=SearchResultsPagination]'))
-					.closest('li').next('li')
-			);
-			return !$a.length ? false : `https://www.airbnb.com${$a.attr('href')}`;
-		});
+			,await n.evaluate(() => {
+				var $ = jQuery;
+				var $a = $('a[aria-label*=Page]',
+					$('a[aria-label*=current]', $('ul[data-id=SearchResultsPagination]'))
+						.closest('li').next('li')
+				);
+				return !$a.length ? false : `https://www.airbnb.com${$a.attr('href')}`;
+			})
+		];
 		await n.end();
-		r = r.concat(flats);
-		curPage++;
-		if (next && curPage < maxPages) {
-			console.log(`next: ${next}`);
-			r = this.page(next, r);
-		}
 		return r;
 	}
 };
